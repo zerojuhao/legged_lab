a5f503a95df37030f0dac66216b309ac8a03f9c6
xiaobaige <zitongbai@outlook.com>
2025-06-13 20:58:50 +0800

    less amp obs, more policy obs

--- source/legged_lab/legged_lab/tasks/locomotion/amp/amp_env_cfg.py
+++ source/legged_lab/legged_lab/tasks/locomotion/amp/amp_env_cfg.py
@@ -29,7 +29,35 @@ from legged_lab.tasks.locomotion.velocity.velocity_env_cfg import (
 
 
 @configclass
-class AmpObservationsCfg(ObservationsCfg):
+class AmpObservationsCfg():
+    
+    @configclass
+    class AugPolicyCfg(ObsGroup):
+        """Observations for policy group."""
+
+        # observation terms (order preserved)
+        base_lin_vel = ObsTerm(func=mdp.base_lin_vel, noise=Unoise(n_min=-0.1, n_max=0.1))
+        base_ang_vel = ObsTerm(func=mdp.base_ang_vel, noise=Unoise(n_min=-0.2, n_max=0.2))
+        projected_gravity = ObsTerm(
+            func=mdp.projected_gravity,
+            noise=Unoise(n_min=-0.05, n_max=0.05),
+        )
+        velocity_commands = ObsTerm(func=mdp.generated_commands, params={"command_name": "base_velocity"})
+        joint_pos = ObsTerm(func=mdp.joint_pos_rel, noise=Unoise(n_min=-0.01, n_max=0.01))
+        joint_vel = ObsTerm(func=mdp.joint_vel_rel, noise=Unoise(n_min=-1.5, n_max=1.5))
+        actions = ObsTerm(func=mdp.last_action)
+        key_links_pos_b: ObsTerm = ObsTerm(
+            func=mdp.key_links_pos, 
+            params={
+                "asset_cfg": SceneEntityCfg("frame_transformer")
+            }
+        )
+        
+        def __post_init__(self):
+            self.enable_corruption = True
+            self.concatenate_terms = True
+    
+    policy: AugPolicyCfg = AugPolicyCfg()
     
     @configclass
     class AmpCfg(ObsGroup):        
@@ -37,7 +65,7 @@ class AmpObservationsCfg(ObservationsCfg):
         dof_vel: ObsTerm = ObsTerm(func=mdp.joint_vel)
         # base_lin_vel_b: ObsTerm = ObsTerm(func=mdp.base_lin_vel)
         # base_ang_vel_b: ObsTerm = ObsTerm(func=mdp.base_ang_vel)
-        base_pos_z: ObsTerm = ObsTerm(func=mdp.base_pos_z)  # TODO: consider terrain height
+        # base_pos_z: ObsTerm = ObsTerm(func=mdp.base_pos_z)  # TODO: consider terrain height
         key_links_pos_b: ObsTerm = ObsTerm(
             func=mdp.key_links_pos, 
             params={
@@ -97,7 +125,7 @@ class LocomotionAmpEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.scene.terrain.terrain_generator = None
         # no height scan
         self.scene.height_scanner = None
-        self.observations.policy.height_scan = None
+        # self.observations.policy.height_scan = None
         # no terrain curriculum
         self.curriculum.terrain_levels = None
         
